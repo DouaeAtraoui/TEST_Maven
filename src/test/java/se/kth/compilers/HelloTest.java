cfb309cd03399466d3e2a74a1716913e5a36cb26
root <d.atraoui@attijariwafa.com>
2024-08-08 14:04:14 +0000

    Test

--- src/test/java/se/kth/compilers/HelloTest.java
+++ src/test/java/se/kth/compilers/HelloTest.java
@@ -1,37 +1,35 @@
 package se.kth.compilers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 class HelloTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-  private final PrintStream standardOut = System.out;
-  private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
-  @BeforeEach
-  public void setUp() {
-    System.setOut(new PrintStream(outputStreamCaptor));
-  }
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 
-  @AfterEach
-  public void tearDown() {
-    System.setOut(standardOut);
-  }
+    @Test
+    public void mainTest_empty() {
+        Hello.main(new String[0]);
+        Assertions.assertEquals("Hello World!", outputStreamCaptor.toString());
+    }
 
-  @Test
-  public void mainTest_empty() {
-    Hello.main(new String[0]);
-    Assertions.assertEquals("Hello World!", outputStreamCaptor.toString());
-  }
-
-  @Test
-  public void mainTest_arg() {
-    Hello.main(new String[] {"Sandra"});
-    Assertions.assertEquals("Hello Sandra!", outputStreamCaptor.toString());
-  }
+    @Test
+    public void mainTest_arg() {
+        Hello.main(new String[] { "Sandra" });
+        Assertions.assertEquals("Hello Sandra!", outputStreamCaptor.toString());
+    }
 }
